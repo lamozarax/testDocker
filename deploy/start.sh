f5f57d1e49443623ff6a416e2c749ae4711df6f5
suming <suming@xiaogu-tech.com>
2017-07-30 13:19:41 +0800

    adjust

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo 'start gunicorn'
-cd /sites/$SITENAME/source/
-gunicorn --bind unix:/tmp/$SITENAME.socket config.wsgi:application --access-logfile /tmp/gunicorn.log
-
 echo 'init db'
 /etc/init.d/postgresql start
 sh /sites/$SITENAME/misc/cleanup.sh
 
 echo 'start nginx'
 /etc/init.d/nginx start
+
+echo 'start gunicorn'
+cd /sites/$SITENAME/source/
+gunicorn --bind unix:/tmp/$SITENAME.socket config.wsgi:application --access-logfile /tmp/gunicorn.log
